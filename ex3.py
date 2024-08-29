8104c60f0342a0a9ffaf2c9b8b4a8765bcc6156b
LRS-Dev <lucaseygor@gmail.com>
2024-08-29 20:44:07 -0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,15 @@
+A = (4/2)+(2/4)
+B = 4/(2+2)/4
+C = (4+2)*2-4	
+if A == 4/2+2/4:
+    print("Correto!!!")
+else:
+    print("Incorreto!!!")
+if B == 4/2+2/4:
+    print("Correto!!!")
+else:
+    print("Incorreto!!!")
+if C == 4+2*2-4:
+    print("Correto!!!")
+else:
+    print("Incorreto!!!")
---
+++ ex6.py
@@ -0,0 +1,3 @@
+B = int(input("digite a base do retângulo ", ))
+H = int(input("digite a altura do retângulo ", ))
+print("então a área do seu retâgulo é ", B * H)
