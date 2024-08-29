7ea41df20760e4041c5b6495aa54d72812e68b6d
LRS-Dev <lucaseygor@gmail.com>
2024-08-29 19:15:46 -0300

    Add files via upload

---
+++ EX2..py
@@ -0,0 +1,18 @@
+A = (4/2)+(2/4)
+print(A, "Resultado da opção A")
+B =  4/(2+2)/4	
+print(B, "Resultado da opção B")
+C =  (4+2)*2-4	
+print(C, "Resultado da opção C")
+if A == 4/2+2/4:
+    print("Correto!!")
+else:
+    print("Errado!!")
+if B == 4/2+2/4:
+    print("Correto")
+else:
+    print("Errado")
+if C == 4+2*2-4:
+    print*("correto")
+else:
+    print("Errado")
---
+++ ex1.py
@@ -0,0 +1,8 @@
+A = 10
+B = 20
+print("Valor de A", A)
+print("Valor de B", B)
+A = 20
+B = 10
+print("Valor de A", A)
+print("Valor de B", B)
